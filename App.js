ee9e4c263b9c0af95ce6c42e20668f80c7b21e07
GiftSibiya <sibiya.bobo@gmail.com>
2024-01-04 23:30:19 +0200

    Added Home Screen

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./navigation";
 
 export default function App() {
-  return (
-    <View className="flex h-screen justify-center items-center bg-orange-500">
-      <Text>The stuff is running as well as it should</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation />;
 }
