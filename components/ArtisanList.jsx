cab1a92c438bfa15b1fea39fbe084b138ea8b78a
GiftSibiya <sibiya.bobo@gmail.com>
2024-01-06 08:03:51 +0200

    work order page

--- components/ArtisanList.jsx
+++ components/ArtisanList.jsx
@@ -2,9 +2,6 @@ import { View, Text, Image } from "react-native";
 import React from "react";
 
 const ArtisanList = ({ artisan }) => {
-  console.log(artisan);
-  console.log(artisan.image);
-
   return (
     <View className="flex flex-row absolute top-[20px] w-[90vw] rounded-xl h-[18vh] bg-white aspect-square overflow-hidden ">
       <Image
@@ -12,12 +9,18 @@ const ArtisanList = ({ artisan }) => {
         source={{ uri: artisan.image }}
       />
       <View className="m-2">
-        <Text>{`${artisan.name} ${artisan.surname}`}</Text>
-        <Text className="text-[10px]">{`${artisan.qualification.qualificationName}`}</Text>
-        <Text className="text-[12px]">{`availability: ${artisan.available}`}</Text>
+        <Text className="font-bold">{`${artisan.name} ${artisan.surname}`}</Text>
+        <Text className="text-[10px] font-bold">{`${artisan.qualification.qualificationName}`}</Text>
+        <Text className="text-[12px]">
+          <Text className="font-bold">availability:</Text>{" "}
+          {` ${artisan.available}`}
+        </Text>
         <Text className="text-[12px]">{`skills: ${artisan.skills}`}</Text>
-        <Text className="text-[12px] mt-4">{`contact: ${artisan.phoneNumber}`}</Text>
-        <Text className="text-[12px] mt-1">View Profile</Text>
+        <Text className="text-[12px] mt-4">
+          <Text className="text-[12px] font-bold">Contact</Text>{" "}
+          {`${artisan.phoneNumber}`}
+        </Text>
+        <Text className="text-[12px] text-blue-600 mt-1">View Profile</Text>
       </View>
     </View>
   );
